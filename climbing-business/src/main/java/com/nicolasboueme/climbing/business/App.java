d21384cef796f2fcf987fe7c2dc99bc9c28d2a2c
Nicolas Bouème <nicolasboueme@gmail.com>
2017-10-30 17:00:28 +0100

    Adding modules with Maven

---
+++ climbing-business/src/main/java/com/nicolasboueme/climbing/business/App.java
@@ -0,0 +1,13 @@
+package com.nicolasboueme.climbing.business;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
